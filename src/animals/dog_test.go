f71686d07b6b5d78ee93a2925585fa2869ca7c73
Nicola Hughes <nicolarhughes@gmail.com>
2017-02-09 22:07:59 +0000

    Add test for dog to bark

--- src/animals/dog_test.go
+++ src/animals/dog_test.go
@@ -1,17 +1,26 @@
 package animals
 
 import (
-  "reflect"
-  "testing"
+	"reflect"
+	"testing"
 )
 
 // go test ./src/animals
 func TestSetHobbiesSetsTheHobbies(t *testing.T) {
-     hobbies := []string{"Barking"}
-     dog := Dog{}
-     dog.SetHobbies(hobbies)
+	hobbies := []string{"Barking"}
+	dog := Dog{}
+	dog.SetHobbies(hobbies)
 
-     if !reflect.DeepEqual(dog.GetHobbies(), hobbies) {
-       t.Fail()
-     }
+	if !reflect.DeepEqual(dog.GetHobbies(), hobbies) {
+		t.Fail()
+	}
+}
+
+func TestDogBarks(t *testing.T) {
+	noise := "bark"
+	dog := Dog{}
+
+	if dog.MakeNoise() != noise {
+		t.Fail()
+	}
 }
